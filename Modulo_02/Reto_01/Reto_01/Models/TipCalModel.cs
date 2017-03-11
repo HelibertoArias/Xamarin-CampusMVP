8ceb239f9ef7942f6f310fe43ae534273b5f966a
helibertoarias <helibertoarias@gmail.com>
2017-03-11 17:40:36 -0500

    Refactor : Modulo_02/Reto_01
    - Add class TipCalModel.

---
+++ Modulo_02/Reto_01/Reto_01/Models/TipCalModel.cs
@@ -0,0 +1,99 @@
+﻿using Reto_01.ViewModels.Base;
+
+namespace Reto_01.Models
+{
+    public class TipCalModel : ViewModelBase
+    {
+        #region Attributes
+
+        private double _food;
+
+        private double _foodTax;
+
+        private int _tipPercent;
+
+        private double _tipAmount;
+
+        private double _total;
+
+        #endregion Attributes
+
+        #region Properties
+
+        public double Food
+        {
+            get { return _food; }
+            set
+            {
+                _food = value;
+                Recalculate();
+                OnPropertyChanged();
+            }
+        }
+
+        public double FoodTax
+        {
+            get { return _foodTax; }
+            set
+            {
+                _foodTax = value;
+                Recalculate();
+                OnPropertyChanged();
+            }
+        }
+
+        public int TipPercent
+        {
+            get { return _tipPercent; }
+            set
+            {
+                _tipPercent = value;
+                Recalculate();
+                OnPropertyChanged();
+            }
+        }
+
+        public double TipAmount
+        {
+            get { return _tipAmount; }
+            set
+            {
+                _tipAmount = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double Total
+        {
+            get { return _total; }
+            set
+            {
+                _total = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion Properties
+
+        public TipCalModel()
+        {
+            Food = 0;
+            FoodTax = 0;
+            TipPercent = 0;
+            TipAmount = 0;
+            Total = 0;
+        }
+
+        public void Recalculate()
+        {
+            TipAmount = 0;
+            Total = 0;
+
+            if (Food > 0 && FoodTax > 0)
+            {
+                TipAmount = (Food * TipPercent) / 100;
+                Total = TipAmount + FoodTax;
+            }
+        }
+    }
+}
--- Modulo_02/Reto_01/Reto_01/ViewModels/MainViewModel.cs
+++ Modulo_02/Reto_01/Reto_01/ViewModels/MainViewModel.cs
@@ -1,105 +1,14 @@
-﻿using Reto_01.ViewModels.Base;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Reto_01.Models;
 
 namespace Reto_01.ViewModels
 {
-    public class MainViewModel : ViewModelBase
+    public class MainViewModel
     {
-        #region Attributes
-        private double _food;
-
-        private double _foodTax;
-
-        private int _tipPercent;
-
-        private double _tipAmount;
-
-        private double _total;
-        #endregion
-
-        #region Properties
-
-        public double Food
-        {
-            get { return _food; }
-            set
-            {
-                _food = value;
-                Recalculate();
-                OnPropertyChanged();
-            }
-        }
-
-        public double FoodTax
-        {
-            get { return _foodTax; }
-            set
-            {
-                _foodTax = value;
-                Recalculate();
-                OnPropertyChanged();
-            }
-        }
-
-        public int TipPercent
-        {
-            get { return _tipPercent; }
-            set
-            {
-                _tipPercent = value;
-                Recalculate();
-                OnPropertyChanged();
-            }
-        }
-
-        public double TipAmount
-        {
-            get { return _tipAmount; }
-            set
-            {
-                _tipAmount = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public double Total
-        {
-            get { return _total; }
-            set
-            {
-                _total = value;
-                OnPropertyChanged();
-            }
-        }
-        #endregion
+        public TipCalModel Model { get; set; }
 
         public MainViewModel()
         {
-            Food = 0;
-            FoodTax = 0;
-            TipPercent = 0;
-            TipAmount = 0;
-            Total = 0;
+            Model = new TipCalModel();
         }
-
-        public void Recalculate()
-        {
-            TipAmount = 0;
-            Total = 0;
-
-            if (Food > 0 && FoodTax > 0)
-            {
-                TipAmount = (Food * TipPercent) / 100;
-                Total = TipAmount + FoodTax;
-            }
-
-        }
-
-
-
     }
 }
